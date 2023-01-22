091285d9203dca6d2001102e3a44ab8d71545679
Dickson Ngan <33165850+kmdicksn@users.noreply.github.com>
2023-01-22 11:56:44 -0800

    last front end

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import {Login, PreferencesPage, ImportCalendar, Loading, Calendar} from "./pages";
 import Button from "@mui/material/Button";
+import httpClient from "./httpClient";
+import {useEffect, useState} from "react";
 
 const theme = createTheme({
     palette: {
@@ -21,17 +23,32 @@ const theme = createTheme({
 });
 
 export default function App() {
+    const [schedule, setSchedule] = useState([]);
+
+    // useEffect(() => {
+    //     const getSchedule = async () => {
+    //         try {
+    //             const resp = await httpClient.get("127.0.0.1:5000/get_schedule");
+    //             setSchedule(resp.data);
+    //         } catch(e) {
+    //             console.log("Failed to get schedule.");
+    //         }
+    //     }
+    //      setSchedule();
+    // }, []);
+
   return (
   <ThemeProvider theme={theme}>
-    <View style={styles.container}>
-            <div style={{
-                backgroundColor: "white",
-                borderRadius: 12,
-                overflow: "hidden",
-            }}>
-                <ImportCalendar />
-            </div>
-    </View>
+      {schedule.length > 0 ? <Calendar schedule={schedule} /> :
+          <View style={styles.container}>
+              <div style={{
+                  backgroundColor: "white",
+                  borderRadius: 12,
+                  overflow: "hidden",
+              }}>
+                  <ImportCalendar setSchedule={setSchedule} />
+          </div>
+      </View>}
   </ThemeProvider>
   );
 }
